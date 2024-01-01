d4f692720339a2c0bf5b923744243fe33e828db8
Fabian Kanhai <fabian.k@ziggo.nl>
2024-01-01 23:15:46 +0100

    Add before hook connection.js

--- mongodb-playground/test/connection.js
+++ mongodb-playground/test/connection.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
+//Connect to the db before tests are run
+before((done) => {
+
 // Connect to Mongodb
 mongoose.connect('mongodb://localhost/testaroo');
 
 mongoose.connection.once('open', () => {
-    console.log('Connection has been Made, let the spectacle run ...');
-}).on('error', (error) => console.log('Connection error..', error));
+
+        console.log('Connection has been Made, let the spectacle run ...');
+        done();
+    })
+    .on('error', (error) => console.log('Connection error..', error));
+})
