fd11a3b7e7c670b1eae53290ce769c02446dcb1a
Fabian Kanhai <fabian.k@ziggo.nl>
2024-01-01 23:18:14 +0100

    save_test.js first test

---
+++ mongodb-playground/test/saving_test.js
@@ -0,0 +1,23 @@
+// const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar  = require('../models/mariochar')
+
+
+// Describe the tests
+describe('Saving records', function() {
+    
+    //Create the tests you want to run
+    it('Saving a record to the database', function(done) {
+        
+        let char = new MarioChar({
+            name: 'Mario'
+        });
+
+        char.save()
+            .then(() => {
+                assert(char.isNew === false);
+                done();
+            })
+    });
+
+})
