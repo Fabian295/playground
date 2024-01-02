0ef640c24ddbe24aeae3a58ed020236c2e06b500
Fabian Kanhai <fabian.k@ziggo.nl>
2024-01-02 05:57:14 +0100

    Update

--- mongodb-playground/test/connection.js
+++ mongodb-playground/test/connection.js
@@ -11,15 +11,12 @@ mongoose.connection.once('open', () => {
         console.log('Connection has been Made, let the spectacle run ...');
         done();
     })
-    .on('error', (error) => console.log('Connection error..', error));
+    .on('error', (error) => console.log('Connection error..:', error));
 })
 
 
 // Drop ( delete ) the characters collection with each test
 beforeEach((done) => {
     // Drop the collection
-    mongoose.connection.collections.mariochars.drop(() => {
-
-        done();
-    });
+    mongoose.connection.collections.mariochars.drop(() => done());
 })
